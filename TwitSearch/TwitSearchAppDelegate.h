855c25e77e9b822c5e4b99a6b21f4b7f9f059c63
Ryan Slade <ryanslade@gmail.com>
2011-09-22 09:47:55 +0100

    Initial Commit

---
+++ TwitSearch/TwitSearchAppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  TwitSearchAppDelegate.h
+//  TwitSearch
+//
+//  Created by slader on 22/09/2011.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TwitSearchAppDelegate : NSObject <UIApplicationDelegate> {
+
+}
+
+@property (nonatomic, retain) IBOutlet UIWindow *window;
+
+@property (nonatomic, retain) IBOutlet UINavigationController *navigationController;
+
+@end
